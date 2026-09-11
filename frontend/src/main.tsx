b9399c5959c07d7fb39b8f33e95abffee754a9ac
Cameron Lee <cameronkmlee10@gmail.com>
2026-09-11 17:50:23 -0400

    feature: add the rest of the frontend onboarding that wasn't staged for some reason

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
+import "./index.css"; // default styles are applied here
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+/**
+ * @brief Main component displaying the main application, which is linked to the index.html file
+ * @return tsx element of Main component
+ */
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
